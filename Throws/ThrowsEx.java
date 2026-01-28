71e24afa4cc8715e7f808b164c2150ac8aaea150
Rikas <rrikas341@gmail.com>
2026-01-28 14:50:47 +0530

    Add files via upload

---
+++ Throws/ThrowsEx.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class ThrowsEx {
+    static void readFile(String fileName) throws IOException {
+        
+        FileReader file = new FileReader(fileName);
+    }
+
+    public static void main(String[] args){
+        
+        try {
+            readFile("test.txt");
+        } catch (IOException e){
+            
+            System.out.println("File not found: " + e.getMessage());
+        }
+    }
+}
